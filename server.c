663b0f17f9fa5131e851c77527dc84dff72881a4
jonahbf <yohan.feldman@gmail.com>
2019-09-18 19:05:42 -0400

    added select loop

--- server.c
+++ server.c
@@ -11,26 +11,68 @@
 #define DEFAULT_PORT_NO 80
 int initialize_listening_sock(int portno);
 void write_response(/*char* fname,*/ int socket);
+int accept_connection(lSock);
 
 
 //TODO: function out all of the lines relevant to accepting a connection
     // TODO: open index.html
     // TODO: write line by line
 
-int main(int arg, char** argv){
-    int lSock;
-    int cliSock;
 
-    struct sockaddr_in cli_addr;
-    socklen_t clilen;
-    clilen = sizeof(cli_addr);
+int main(int arg, char** argv){
+    int lSock, maxSock, rv, newSock = -1;
+
+    struct timeval tv;
+    tv.tv_sec=1;
+    tv.tv_usec=1000;
+    fd_set masterFDSet, copyFDSet;
+    FD_ZERO(&masterFDSet);
+    FD_ZERO(&copyFDSet);
+    FD_SET(lSock, &masterFDSet);
+
+    lSock = initialize_listening_sock(DEFAULT_PORT_NO);
+    maxSock = lSock;
+
+    while (1){
+        FD_ZERO(&copyFDSet);
+        memcpy(&copyFDSet, &masterFDSet, sizeof(masterFDSet));
+        rv = select (maxSock + 1, &copyFDSet, NULL, NULL, &tv);
+   
+        if (rv == -1)
+            perror("Select");
+        else if (rv ==0)
+            ;
+        else{
+            for (int sockfd = 0; sockfd < maxSock + 1; sockfd++){
+                //if there is any connection or new data to be read
+                if ( FD_ISSET (sockfd, &copyFDSet) ) {
+                    if (sockfd == lSock){
+                        newSock = accept_connection(lSock);
+                        if (newSock > 0){
+                            FD_SET(newSock, &masterFDSet);
+                            maxSock = (newSock > maxSock) ? newSock: maxSock;
+                            //add to sockArray
+                        }
+                        
+                    }
+                    else{
+                        write_response(sockfd);
+                        if (status == DISCONNECT_CODE || 1 /*delete the or 1, rn always disconnects client */){
+                            //TODO: write a disconnect function
+                            close(sockfd);
+                            FD_CLR(sockfd, masterFDSet);
+                            
+                            
+                        }
+
+                    }
+                }
+            }
+        }
+    }
 
     
-    lSock = initialize_listening_sock(DEFAULT_PORT_NO);
-    cliSock = accept(lSock, (struct sockaddr *) &cli_addr,  &clilen);
     //fprintf(stderr, "accepted new socket with sockfd %d\n", cliSock);
-    write_response(cliSock);
-    close(cliSock);
     return 0;
 
 }
@@ -44,7 +86,6 @@ int initialize_listening_sock(int portno){
     
     
     struct sockaddr_in serv_addr;
-    int n;
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) 
        perror("ERROR opening socket");
@@ -72,5 +113,18 @@ void write_response(/*char* fname,*/int socket){
     else{
         fprintf(stderr, "wrote message of %d bytes\n", n);
     }
+
+}
+
+/* Purpose: To accept an incoming connection from a new client
+ * Args: listening socket file descriptor
+ * Returns: socket file descriptor for new client
+ */
+int accept_connection(lSock){
+    struct sockaddr_in cli_addr;
+    socklen_t clilen;
+    clilen = sizeof(cli_addr);
+
+    return accept(lSock, (struct sockaddr *) &cli_addr,  &clilen);
 
 }
