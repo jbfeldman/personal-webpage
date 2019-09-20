fa4ff3c091f1fe25301c1bf5da12fe34cfcbdf8a
jonahbf <yohan.feldman@gmail.com>
2019-09-19 21:15:00 -0400

    major bug fixes, now working

--- HTTP-parser.c
+++ HTTP-parser.c
@@ -3,21 +3,17 @@
 void get_first_line (struct HTTP_request* header, char *buffer);
 void get_field(char **field, char *field_string, char *buffer);
 
-struct HTTP_request* parse_request(char* buffer, int n){
+struct HTTP_request* parse_request(char* buffer){
     struct HTTP_request* header = malloc(sizeof(struct HTTP_request));
 
-    buffer = get_first_line(header, buffer);
+    get_first_line(header, buffer);
     get_field(&(header->host), "Host: ", buffer);
     // can add more fields here
 
-
-    // header->type = strdup("dummy thicc type");
-    // header->url = strdup("pics/dummer/thiccer");
-    // header->host = strdup("dummy.thic.com");
     return header;
 }
 int make_HTTP_response_header(struct HTTP_response response_header, char** buffer){
-    ;
+    return 0;
 }
 void free_HTTP_request(struct HTTP_request* request){
     ;
@@ -40,8 +36,8 @@ void get_first_line (struct HTTP_request* header, char *buffer){
     //create seperate buffer so strtok doesn't malform header string
 
     char line_buffer[line_length];
-    bzero(line_buffer, line_length)
-    strncpy(line_buffer, start_of_line, line_length);
+    bzero(line_buffer, line_length);
+    strncpy(line_buffer, buffer, line_length);
 
     char *temp;
 
@@ -69,44 +65,48 @@ void get_first_line (struct HTTP_request* header, char *buffer){
 void get_field(char **field, char *field_string, char *buffer){
     char* start_of_line = strstr(buffer, field_string);
 
+
     if (start_of_line == NULL){
-        field* = NULL;
+        *field = NULL;
         return;
     } 
     // char* end_of_line = strstr(start_of_line, "\r\n");
     // if (end_of_line == NULL) return NULL;
     if (strstr(start_of_line, "\r\n") == NULL){
-        field* = NULL;
+        *field = NULL;
         return;
     }
 
-    int line_length = strstr(start_of_line, "\r\n") - start_of_line;
+    int line_length = strstr(start_of_line, "\n") - start_of_line;
 
     //create seperate buffer so strtok doesn't malform header string
 
     char line_buffer[line_length];
-    bzero(line_buffer, line_length)
+    char *temp;
+    bzero(line_buffer, line_length);
     strncpy(line_buffer, start_of_line, line_length);
+    // fprintf(stderr, "start of line is %s\n", start_of_line);
+    // fprintf(stderr, "line buffer is %s\n", line_buffer);
+    // fprintf(stderr, "line length is %d\n", line_length);
 
-    if( strtok(line_buffer, ":") == NULL){
-        field* = NULL;
-        return;
-
-    }
-        
-    if (strtok(NULL, " ") == NULL){
-        field* = NULL;
-        return;
-    }
-    line_buffer = strtok(NULL, "\r");
-
-    if (line_buffer == NULL){
-        field* = NULL;
+    temp = strtok(line_buffer, " ");
+    // fprintf(stderr, "temp is %s\n", temp);
+    if (temp == NULL){
+        // fprintf(stderr, "got set to null bc strtok(line_buffer, \" \") was null\n");
+        *field = NULL;
         return;
     }
+    temp = strtok(NULL, "\r");
+    // fprintf(stderr, "temp is %s\n", temp);
+
+    if (temp == NULL){
+        // fprintf(stderr, "got set to null bc strtok(line_buffer, \"r\") was null\n");
+        *field = NULL;
+        return;
+    }
 
 
-    field* = calloc(strlen(line_buffer) + 1, sizeof(char));
-    strncpy(field, line_buffer, strlen(line_buffer));
+    *field = calloc(strlen(temp) + 1, sizeof(char));
+    strncpy(*field, temp, strlen(temp));
 
 }
