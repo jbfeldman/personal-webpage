72fbb720a95fd66286d41361b94b5c5bdb096d1d
jonahbf <yohan.feldman@gmail.com>
2019-09-25 17:12:08 -0400

    removed some annoying warnings

--- HTTP-compile.sh
+++ HTTP-compile.sh
@@ -1,1 +1,3 @@
-gcc HTTP-test.c io.c HTTP-parser.c -Wall -Wextra -o parser
+#gcc HTTP-test.c io.c HTTP-parser.c -Wall -g -Wextra -o parser
+#without any warnings
+gcc HTTP-test.c io.c HTTP-parser.c -g -o parser
